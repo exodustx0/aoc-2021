9efbc975d7ac830c537d736d73d7a946190aa575
exodustx0 <m.v@exodu.st>
2021-12-04 14:47:54 +0100

    Day 4

--- README.md
+++ README.md
@@ -13,3 +13,25 @@ I think the setup I've got here is best, Go module-wise. I had some trouble with
 It took me an annoyingly long time to debug for part two. It was a few hours in when I _finally_ noticed that I forgot a `break` statement. Figures!
 
 My clumsiness of the day aside (it took me too long to pull in the example input and test against _that_), I've got mixed feelings on how the debugging process felt, in terms of how doable it was with the tools that Go offers. No conclusions as of yet, but I'll be paying special attention from here on out.
+
+### Day 4
+
+This repo is now public. What the heck, might as well. Not like I'm a contender for the global leaderboard anyway :P
+
+I loved today's challenge. I'm not trying to be done as fast as possible; rather, I try to learn where I feel that I can do better, to see where I can break free from thinking about problems in the usual chronological order (e.g. rather than mark every bingo board after every call, do the entire call sequence per board), etc. I really working with Go struct methods, and though I don't have much previous experience with pointers (only through studying certain big C++ projects), I really find that I love working with them and can do so intuitively.
+
+On the flipside, Go's opinionatedness has really shown it's — in my opinion — ugly side today, specifically in how it "beautifies" math expressions. Take this line that I ended up with:
+
+```go
+(*boards)[lastBoard].unmarkedSum()*int((*calls)[lastCall])
+```
+
+Dunno about you, but that reads like yikes to me. I'm a big proponent for space-separating tokens in expressions like these. Even though the difference would be only two spaces, look:
+
+```go
+(*boards)[lastBoard].unmarkedSum() * int((*calls)[lastCall])
+```
+
+Especially since `*` is used for both multiplying and dereferencing, this is _much_ more legible to me. Note that Go will format to this only when this expression is _one_ of the arguments to a function call; for some reason, however, it'll actually format to the _space-separated_ version if it's the only argument in a function call. I guess that Go wants spaces to separate both arguments and expression tokens. One space-separation to rule them all.
+
+(Also, in case it comes up at some point: I don't mind going back and reworking previous days' code. I'm a perfectionist. Posterity can still have the commit history!)
---
+++ day-4/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Cell struct {
+	value  byte
+	marked bool
+}
+type Board struct {
+	cells  [5][5]Cell
+	marks  byte
+	hasWon bool
+}
+
+func (board *Board) mark(value byte) {
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			if board.cells[y][x].value == value {
+				board.cells[y][x].marked = true
+				board.marks++
+				if board.marks >= 5 {
+					board.checkWin()
+				}
+				return
+			}
+		}
+	}
+}
+
+func (board *Board) checkWin() {
+	for x := 0; x < 5; x++ {
+		for y := 0; y < 5; y++ {
+			if !board.cells[y][x].marked {
+				break
+			}
+
+			if y == 4 {
+				board.hasWon = true
+				return
+			}
+		}
+	}
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			if !board.cells[y][x].marked {
+				break
+			}
+
+			if x == 4 {
+				board.hasWon = true
+				return
+			}
+		}
+	}
+}
+
+func (board *Board) unmarkedSum() (sum int) {
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			if !board.cells[y][x].marked {
+				sum += int(board.cells[y][x].value)
+			}
+		}
+	}
+	return
+}
+
+func getInput() (*[]byte, *[]*Board) {
+	// f, err := os.Open("example.txt")
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+
+	calls := make([]byte, 0)
+	boards := make([]*Board, 0)
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	for _, valueStr := range strings.Split(scanner.Text(), ",") {
+		value, _ := strconv.Atoi(valueStr)
+		calls = append(calls, byte(value))
+	}
+
+	var x, y int
+	var board *Board
+	for scanner.Scan() {
+		if x == 0 && y == 0 {
+			board = new(Board)
+		}
+
+		value, _ := strconv.Atoi(scanner.Text())
+		board.cells[y][x].value = byte(value)
+
+		x++
+		if x == 5 {
+			x = 0
+			y++
+			if y == 5 {
+				y = 0
+			}
+		}
+
+		if x == 0 && y == 0 {
+			boards = append(boards, board)
+		}
+	}
+
+	if x != 0 || y != 0 {
+		panic("Incomplete board")
+	}
+
+	return &calls, &boards
+}
+
+func partOne() {
+	calls, boards := getInput()
+
+	for _, value := range *calls {
+		for _, board := range *boards {
+			board.mark(value)
+			if board.hasWon {
+				fmt.Println("Part one:", board.unmarkedSum()*int(value))
+				return
+			}
+		}
+	}
+}
+
+func partTwo() {
+	calls, boards := getInput()
+
+	var lastBoard, lastCall int
+	for i, board := range *boards {
+		for call, value := range *calls {
+			board.mark(value)
+			if board.hasWon {
+				if call > lastCall {
+					lastBoard = i
+					lastCall = call
+				}
+				break
+			}
+		}
+	}
+
+	fmt.Println("Part two:", (*boards)[lastBoard].unmarkedSum()*int((*calls)[lastCall]))
+
+	fmt.Println((*boards)[lastBoard].unmarkedSum() * int((*calls)[lastCall]))
+}
+
+func main() {
+	partOne()
+	partTwo()
+}
